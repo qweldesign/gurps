82557c634d565dcbbaf6f619d11213a10bc71c15
QWEL.DESIGN <webmaster@qwel.design>
2026-03-11 23:10:11 +0900

    refactor: LocalStorage を使用する Hook を追加

--- src/parts/Edit.tsx
+++ src/parts/Edit.tsx
@@ -1,5 +1,6 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+import { useLocalStorage } from '../hooks/useLocalStorage'
 import List from './Edit/List'
 import Detail from './Edit/Detail'
 import { type CharacterData, Character } from '../lib/domains/Character'
@@ -11,33 +12,52 @@ function Edit() {
 
   // LocalStorage を使用
   const storageKey = 'savedata';
-
-  // LocalStrage が空の場合はサンプルキャラクターを生成する
-  const initData = (): CharacterData[] => {
-    const mod = Math.floor(Math.random() * 15) // 乱数 0～15 を足してサンプル生成
-    const data = createSamples(10, 1, 4, mod).map(sample => sample.toData())
-    localStorage.setItem(storageKey, JSON.stringify(data))
-    return data
-  }
-
-  // LocalStrage の初期化
-  const loadModels = (): Character[] => {
-    const stored = localStorage.getItem(storageKey)
-    const data:CharacterData[] = stored ? JSON.parse(stored) : initData()
-    return data
-      .sort((a, b) => a.id - b.id) // IDでソート
-      .map(dt => new Character(dt))
-  }
+  
+  // LocalStorage のインデックス (uid配列を格納)
+  const [list, setList] = useLocalStorage<string[]>(`${storageKey}:index`, [])
 
   // List, Detail に渡すパラメータ
-  const [models, setModels] = useState<Character[]>(loadModels)
+  const [models, setModels] = useState<Character[]>([])
 
-  // ゲームの初期化
+  // LocalStorage が空の場合はサンプルキャラクターを生成する
+  const initModels = (): Character[] => {
+    const mod = Math.floor(Math.random() * 15) // 乱数 0～15 を足してサンプル生成
+    const models = createSamples(10, 1, 4, mod)
+    return models
+  }
+
+  // ゲームの初期化 (初期表示)
   const reset = () => {
     localStorage.clear()
-    const data: CharacterData[] = initData()
-    setModels(data.map(dt => new Character(dt)))
+    // サンプル生成
+    const samples = initModels()
+    const uids = samples.map((sample, i) => {
+      const uid = `${i + 1}`.padStart(2, '0') // ID文字列生成
+      // シリアライズ用データ変換の上 LocalStorage にキャラクターを保存
+      localStorage.setItem(`${storageKey}:${uid}`, JSON.stringify(sample.toData()))
+      return uid
+    })
+    setList(uids) // LocalStorage にインデックスを保存
   }
+
+  useEffect(() => {
+    if (list.length === 0) {
+      reset() // インデックスが見つからない場合はリセット
+    } else {
+      const next = list.sort().map(key => {
+        const stored = localStorage.getItem(`${storageKey}:${key}`)
+        const data = stored ? JSON.parse(stored) : {
+          id: 0,
+          name: '未設定',
+          gender: '男性',
+          points: [],
+          equipments: null
+        } as CharacterData
+        return new Character(data)
+      })
+      setModels(next)
+    }
+  }, [list])
 
   return (
     <>
