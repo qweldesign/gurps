a380b8eb55dd8458b19589f061e08ac904a94a7d
QWEL.DESIGN <webmaster@qwel.design>
2026-03-16 01:22:19 +0900

    chore: 初期リリース前に開発途上項目を調整
    - 模擬戦闘
    - 初期作成時のCP/所持金設定
    - サンプルキャラクターの表示

--- src/parts/Entrance.tsx
+++ src/parts/Entrance.tsx
@@ -13,9 +13,6 @@ function Entrance() {
         <Link to="/docs/">ドキュメント</Link>
       </li>
       <li className='nav__item'>
-        <Link to="/sample/">サンプル</Link>
-      </li>
-      <li className='nav__item'>
         <Link to={edit}>編成</Link>
       </li>
       <li className='nav__item'>
