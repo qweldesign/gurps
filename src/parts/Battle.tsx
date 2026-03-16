ce008fbb541878261c6aa09b6c1dfd864cf5019b
QWEL.DESIGN <webmaster@qwel.design>
2026-03-16 14:29:25 +0900

    feat: 開発進捗ビューを埋め込み

--- src/parts/Battle.tsx
+++ src/parts/Battle.tsx
@@ -1,8 +1,16 @@
+import DevProgress from './DevProgress'
+import { COMBAT_DEV_PROGRESS } from '../devProgress/combat'
+
 function Battle() {
   return (
-    <div className="row">
-      <p>開発中</p>
-    </div>
+    <>
+      <div className="p-6">
+        <p>In development...</p>
+      </div>
+      <div className="mt-24 px-6">
+        <DevProgress progress={COMBAT_DEV_PROGRESS} />
+      </div>
+    </>
   )
 }
 
