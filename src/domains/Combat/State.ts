327f090391c69906e882d7948a151d22411a7902
Taigo Ito <qwel.design@gmail.com>
2026-08-15 11:09:40 +0900

    feat: ターンとログの仮実装 (移動の実装にて検証)

--- src/domains/Combat/State.ts
+++ src/domains/Combat/State.ts
@@ -1,7 +1,9 @@
 // Combat/State.ts
 
+import { CombatLog as Log } from './Log'
 import { type CombatUnitModel as Model, CombatUnit as Unit } from './Unit'
 import { CombatFormationStore as FormationStore } from './Stores/FormationStore'
+import { CombatActionStore as ActionStore } from './Stores/ActionStore'
 
 // 全ての情報を集約・管理するクラス
 export class CombatState {
@@ -9,14 +11,20 @@ export class CombatState {
   public turnIndex: number // 行動順
   public units: Unit[]
   public formationStore: FormationStore | null
+  public logs: Log[]
+  public playLog: () => Promise<void> // Combat 本体から受け取り, ActionStore から呼び出す
+  public actionStore: ActionStore | null
 
-  constructor(models: Model[]) {
+  constructor(models: Model[], playLog: () => Promise<void>) {
     this.round = 1 // 1からカウント
     this.turnIndex = -1 // 開幕前は -1, 開幕と同時に 0 になる
     this.units = models.map((model, i) => {
       return new Unit(model, i + 1) // combatIdは1からカウント
     })
     this.formationStore = null
+    this.logs = []
+    this.playLog = playLog
+    this.actionStore = null
   }
 
   get actor() {
@@ -31,6 +39,19 @@ export class CombatState {
       this.turnIndex %= this.units.length
     }
     this.formationStore = new FormationStore(this.actor, this.units)
+    // 新しいログを追加
+    const newLog = new Log(this.actor)
+    this.logs.unshift(newLog)
+    // ターン開始ログを表示
+    await this.playLog()
+    // コマンドパレット初期化
+    this.actionStore = new ActionStore(this)
+    //　コマンド入力待機
+    await this.actionStore.promise.then(() => {
+      // 自身を呼び出し, また次のターンへ進む
+      this.debug()
+      this.nextTurn()
+    })
   }
 
   debug() {
