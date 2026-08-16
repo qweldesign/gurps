027619523fef838e2b3b0a6c31589fa6a1022f1a
Taigo Ito <qwel.design@gmail.com>
2026-08-16 13:42:12 +0900

    feat: 全力攻撃/部位狙いオプションを定義

--- src/domains/Combat/Action/types.ts
+++ src/domains/Combat/Action/types.ts
@@ -1,5 +1,6 @@
 // Combat/Action/types.ts
 
+import { type ArmorSlotKey } from '../../Equipments'
 import { type Position } from '../Unit'
 
 export const ACTION_KEYS = ['move', 'wait'] as const
@@ -16,8 +17,39 @@ export const POSITION_LABELS: Record<Position, string> = {
   right: '右翼'
 } as const
 
+export const FULL_POWER_KEYS = ['none', 'dmg', 'level', 'feint', 'double'] as const
+
+export const FULL_POWER_OPTIONS: Record<FullPower, { label: string }> = {
+  none: { label: '通常攻撃' },
+  dmg: { label: 'ダメージ安定' },
+  level: { label: '技能値+4' },
+  feint: { label: '牽制即攻撃' },
+  double: { label: '2回攻撃' }
+} as const
+
+export const AIM_KEYS = ['head', 'ear', 'eye', 'body', 'neck', 'stomach', 'arm', 'hand', 'leg', 'foot'] as const
+
+export const AIM_OPTIONS: Record<Aim, { label: string, group: ArmorSlotKey, mod: number }> = {
+  head: { label: '頭', group: 'head', mod: -3 }, 
+  ear: { label: '耳', group: 'head', mod: -5 },
+  eye: { label: '目', group: 'head', mod: -7 },
+  body: { label: '体', group: 'body', mod: 0 },
+  neck: { label: '喉', group: 'body', mod: -5 },
+  stomach: { label: '肚', group: 'body', mod: -3 },
+  arm: { label: '腕', group: 'arm', mod: -2 },
+  hand: { label: '手首', group: 'arm', mod: -4 },
+  leg: { label: '脚', group: 'leg', mod: -2 },
+  foot: { label: '足首', group: 'leg', mod: -4 }
+} as const
+
 // 行動キー
 export type ActionKey = typeof ACTION_KEYS[number]
+
+// 全力攻撃オプション
+export type FullPower = typeof FULL_POWER_KEYS[number]
+
+// 部位狙いオプション
+export type Aim = typeof AIM_KEYS[number]
 
 // 行動オプション
 export type ActionOptions = {
