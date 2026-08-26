6f66bb07af99c8c8fa9cdcb0d6a89a8f7e8179e2
Taigo Ito <qwel.design@gmail.com>
2026-08-27 08:41:32 +0900

    fix: 幻惑状態での装備・姿勢変更禁止を解除

--- src/domains/Combat/Action/availability.ts
+++ src/domains/Combat/Action/availability.ts
@@ -125,20 +125,19 @@ export class ActionAvailability {
   }
 
   //「装備変更」実行可否取得
-  // sub, spare のいずれかに武器を持っていること, かつ幻惑状態ではないことが条件 (他に持ち替え先の武器が無ければ非表示)
+  // sub, spare のいずれかに武器を持っていること (他に持ち替え先の武器が無ければ非表示)
   canChangeWeapon(): boolean {
     const isSingle = (['sub', 'spare'] as const).every(key => this.state.actor.attack.getModelByKey(key).name === '装備無し')
-    return !isSingle && !this.isDazed()
+    return !isSingle
   }
 
   //「姿勢変更」実行可否取得
   // 直立 → 這い は不可能
   // 這い → 膝着きのみ可能
   // 脚・足首を故障している間は, 杖のような支えが無い前提のため, 二度と「直立」「屈み」には戻れない
-  // その他, 現行の姿勢以外にはいつでも変更可能 (幻惑状態では一切変更不可)
+  // その他, 現行の姿勢以外にはいつでも変更可能
   canChangePosture(posture: Posture): boolean {
     const actor = this.state.actor
-    if (this.isDazed()) return false
     if (actor.health.injuryOnLeg && (posture === 'standing' || posture === 'crouching')) return false
     const current = actor.posture
     if (posture === current) return false
